285ed7e6cf884aa89291e9d21b2b8579acdf6f9d
Thomas Kirkman-Wood <tom@tkw.bz>
2024-01-28 22:36:33 +0000

    Randomisation

--- server/Maths/index.js
+++ server/Maths/index.js
@@ -1,4 +1,7 @@
-const { Config } = require('../Config');
+/**
+ * A module that provides various mathematical functions.
+ * @module Maths
+ */
 
 const Maths = {};
 
@@ -7,6 +10,17 @@ const Maths = {};
  *
  * @param {number[]} InputArray - The array of numbers to calculate the sum.
  * @returns {number} The sum of the numbers in the array.
+ */
+Maths.SumArray = (InputArray) => {
+    if (InputArray.length === 0) return 0;
+    return InputArray.reduce((acc, val) => acc + val, 0);
+}
+
+/**
+ * Calculates the average of an array of numbers.
+ *
+ * @param {number[]} InputArray - The array of numbers to calculate the average.
+ * @returns {number} The average of the numbers in the array.
  */
 Maths.AverageArray = (InputArray) => {
     if (InputArray.length === 0) return 0;
@@ -34,6 +48,30 @@ Maths.MinArray = (InputArray) => {
 Maths.MaxArray = (InputArray) => {
     if (InputArray.length === 0) return undefined;
     return Math.max(...InputArray);
+}
+
+/**
+ * Generates a random floating-point number between the specified minimum and maximum values.
+ *
+ * @param {number} min - The minimum value.
+ * @param {number} max - The maximum value.
+ * @returns {number} A random floating-point number between the minimum and maximum values.
+ */
+Maths.RandomArbitrary = (min, max) => {
+    return Math.random() * (max - min) + min;
+}
+
+/**
+ * Generates a random integer between the specified minimum and maximum values.
+ *
+ * @param {number} min - The minimum value.
+ * @param {number} max - The maximum value.
+ * @returns {number} A random integer between the minimum and maximum values.
+ */
+Maths.RandomInt = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 module.exports = { Maths };
