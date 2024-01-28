aa47563574f4a16d0c5f9b045d549bb7ce5f93a6
Thomas Kirkman-Wood <tom@tkw.bz>
2024-01-28 22:25:04 +0000

    Updating exports

--- server/Buckets/index.js
+++ server/Buckets/index.js
@@ -9,6 +9,8 @@ let EntityLockdownModes = [
     "inactive", // Clients can create any entity they want.
 ]
 
+const Buckets = {};
+
 /**
  * Represents a routing bucket.
  * 
@@ -26,9 +28,6 @@ class RoutingBucket {
         this.ID = ID;
         this.PopulationEnabled = PopulationEnabled || false;
         this.EntityLockdownMode = EntityLockdownMode || "inactive"
-
-        this.Players = new Set();
-        this.Entities = new Set();
 
         SetRoutingBucketEntityLockdownMode(this.ID, this.EntityLockdownMode);
         SetRoutingBucketPopulationEnabled(this.ID, this.PopulationEnabled);
@@ -48,7 +47,7 @@ class RoutingBucket {
  * 
  * @returns {Array} An array containing the routing buckets.
  */
-function GetRoutingBuckets() {
+Buckets.GetRoutingBuckets = () => {
     return Array.from(RoutingBuckets.values());
 }
 
@@ -58,8 +57,19 @@ function GetRoutingBuckets() {
  * @param {number} src - The source of the player.
  * @returns {void}
  */
-function ClearPlayerRoutingBucket(src) {
+Buckets.ClearPlayerRoutingBucket = (src) => {
     SetPlayerRoutingBucket(src, DefaultRoutingBucket);
+    return
+}
+
+/**
+ * Clears the entity routing bucket and sets it to the default routing bucket.
+ * 
+ * @param {number} entity - The source of the player.
+ * @returns {void}
+ */
+Buckets.ClearPlayerRoutingBucket = (entity) => {
+    SetEntityRoutingBucket(entity, DefaultRoutingBucket);
     return
 }
 
@@ -70,7 +80,7 @@ function ClearPlayerRoutingBucket(src) {
  * @param {string} EntityLockdownMode - The entity lockdown mode for the routing bucket. ['strict', 'relaxed', 'inactive']
  * @returns {number} The ID of the created routing bucket.
  */
-function CreateRoutingBucket(PopulationEnabled, EntityLockdownMode) {
+Buckets.CreateRoutingBucket = (PopulationEnabled, EntityLockdownMode) => {
     if (EntityLockdownMode && !EntityLockdownModes.includes(EntityLockdownMode)) return InternalLogger.error(`Invalid EntityLockdownMode '${EntityLockdownMode}'`)
     let ID = GetLowestUnusedRoutingBucket();
     RoutingBuckets.set(ID, new RoutingBucket(ID, PopulationEnabled, EntityLockdownMode))
@@ -83,7 +93,7 @@ function CreateRoutingBucket(PopulationEnabled, EntityLockdownMode) {
  * @param {string} ID - The ID of the routing bucket.
  * @returns {object|null} - The routing bucket object if found, or null if not found.
  */
-function GetRoutingBucket(ID) {
+Buckets.GetRoutingBucket = (ID) => {
     return RoutingBuckets.has(ID) ? RoutingBuckets.get(ID) : null;
 }
 
@@ -94,16 +104,36 @@ function GetRoutingBucket(ID) {
  * @param {string} ID - The ID of the routing bucket.
  * @returns {void} - The routing bucket object if found, or null if not found.
  */
-function DeleteRoutingBucket(ID) {
+Buckets.DeleteRoutingBucket = (ID) => {
     if (!RoutingBuckets.has(ID)) return false;
     let Bucket = RoutingBuckets.get(ID);
-    Bucket.Players.forEach(Player => {
-        SetPlayerRoutingBucket(Player, DefaultRoutingBucket);
-    })
-    Bucket.Entities.forEach(Entity => {
-        SetEntityRoutingBucket(Entity, DefaultRoutingBucket);
-    })
     RoutingBuckets.delete(ID);
+    return
+}
+
+/**
+ * Sets a player's routing bucket.
+ * 
+ * @param {number} src - The player source.
+ * @param {string} BucketID - The ID of the routing bucket.
+ */
+Buckets.SetPlayerBucket = (src, BucketID) => {
+    let Bucket = RoutingBuckets.get(BucketID);
+    if (!Bucket) RoutingBuckets.set(ID, new RoutingBucket(ID, true))
+    SetPlayerRoutingBucket(src, BucketID);
+    return
+}
+
+/**
+ * Sets an entities routing bucket.
+ * 
+ * @param {number} entity - The entity handle.
+ * @param {string} BucketID - The ID of the routing bucket.
+ */
+Buckets.SetEntityBucket = (entity, BucketID) => {
+    let Bucket = RoutingBuckets.get(BucketID);
+    if (!Bucket) RoutingBuckets.set(ID, new RoutingBucket(ID, true))
+    SetPlayerRoutingBucket(entity, BucketID);
     return
 }
 
@@ -121,10 +151,5 @@ function GetLowestUnusedRoutingBucket() {
 }
 
 module.exports = {
-    GetRoutingBuckets,
-    ClearPlayerRoutingBucket,
-
-    CreateRoutingBucket,
-    GetRoutingBucket,
-    DeleteRoutingBucket,
+    Buckets,
 };
