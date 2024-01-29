9f75cf9df57065fbcf3d1a57b17309563d7bb5a0
Thomas Kirkman-Wood <tom@tkw.bz>
2024-01-29 04:32:29 +0000

    feature: Maths.FormatAsCurrency

--- server/Maths/index.js
+++ server/Maths/index.js
@@ -12,9 +12,9 @@ const Maths = {};
  * @returns {number} The sum of the numbers in the array.
  */
 Maths.SumArray = (InputArray) => {
-    if (InputArray.length === 0) return 0;
-    return InputArray.reduce((acc, val) => acc + val, 0);
-}
+	if (InputArray.length === 0) return 0;
+	return InputArray.reduce((acc, val) => acc + val, 0);
+};
 
 /**
  * Calculates the average of an array of numbers.
@@ -23,9 +23,9 @@ Maths.SumArray = (InputArray) => {
  * @returns {number} The average of the numbers in the array.
  */
 Maths.AverageArray = (InputArray) => {
-    if (InputArray.length === 0) return 0;
-    const sum = InputArray.reduce((acc, val) => acc + val, 0);
-    return sum / InputArray.length;
+	if (InputArray.length === 0) return 0;
+	const sum = InputArray.reduce((acc, val) => acc + val, 0);
+	return sum / InputArray.length;
 };
 
 /**
@@ -35,9 +35,9 @@ Maths.AverageArray = (InputArray) => {
  * @returns {number|undefined} The minimum value in the array, or undefined if the array is empty.
  */
 Maths.MinArray = (InputArray) => {
-    if (InputArray.length === 0) return undefined;
-    return Math.min(...InputArray);
-}
+	if (InputArray.length === 0) return undefined;
+	return Math.min(...InputArray);
+};
 
 /**
  * Finds the maximum value in an array of numbers.
@@ -46,9 +46,9 @@ Maths.MinArray = (InputArray) => {
  * @returns {number|undefined} The maximum value in the array, or undefined if the array is empty.
  */
 Maths.MaxArray = (InputArray) => {
-    if (InputArray.length === 0) return undefined;
-    return Math.max(...InputArray);
-}
+	if (InputArray.length === 0) return undefined;
+	return Math.max(...InputArray);
+};
 
 /**
  * Generates a random floating-point number between the specified minimum and maximum values.
@@ -58,8 +58,8 @@ Maths.MaxArray = (InputArray) => {
  * @returns {number} A random floating-point number between the minimum and maximum values.
  */
 Maths.RandomArbitrary = (min, max) => {
-    return Math.random() * (max - min) + min;
-}
+	return Math.random() * (max - min) + min;
+};
 
 /**
  * Generates a random integer between the specified minimum and maximum values.
@@ -69,10 +69,10 @@ Maths.RandomArbitrary = (min, max) => {
  * @returns {number} A random integer between the minimum and maximum values.
  */
 Maths.RandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 /**
  * Generates a boolean with a 50% chance of being true.
@@ -80,8 +80,8 @@ Maths.RandomInt = (min, max) => {
  * @returns {boolean} A random boolean.
  */
 Maths.RandomBool = () => {
-    return Math.random() < 0.5;
-}
+	return Math.random() < 0.5;
+};
 
 /**
  * Calculates the Euclidean distance between two points in a 2D plane.
@@ -96,8 +96,7 @@ Maths.Distance2D = (X1, Y1, X2, Y2) => {
 	let X = X2 - X1;
 	let Y = Y2 - Y1;
 	return Math.sqrt(X * X + Y * Y);
-}
-
+};
 
 /**
  * Calculates the Euclidean distance between two points in a 2D plane.
@@ -110,7 +109,7 @@ Maths.DistanceVector2 = (Pos1, Pos2) => {
 	let X = Pos2.x - Pos1.x;
 	let Y = Pos2.y - Pos1.y;
 	return Math.sqrt(X * X + Y * Y);
-}
+};
 
 /**
  * Calculates the 3D distance between two positions.
@@ -128,7 +127,7 @@ Maths.Distance3D = (X1, Y1, Z1, X2, Y2, Z3) => {
 	let Y = Y2 - Y1;
 	let Z = Z3 - Z1;
 	return Math.sqrt(X * X + Y * Y + Z * Z);
-}
+};
 
 /**
  * Calculates the Euclidean distance between two positions in a 3D plane.
@@ -142,43 +141,41 @@ Maths.DistanceVector3 = (Pos1, Pos2) => {
 	let Y = Pos2.y - Pos1.y;
 	let Z = Pos2.z - Pos2.z;
 	return Math.sqrt(X * X + Y * Y + Z * Z);
-}
+};
 
 /**
  * Represents a 2D vector.
- * 
+ *
  * @param {number} X - The X coordinate of the vector.
  * @param {number} Y - The Y coordinate of the vector.
  * @returns {Object} - The 2D vector object.
  */
 Maths.Vector2 = (X, Y) => {
-    return {
-        x: X,
-        y: Y,
-    }
-}
-
-
+	return {
+		x: X,
+		y: Y,
+	};
+};
 
 /**
  * Represents a 3D vector.
- * 
+ *
  * @param {number} X - The X coordinate of the vector.
  * @param {number} Y - The Y coordinate of the vector.
  * @param {number} Z - The Z coordinate of the vector.
  * @returns {Object} - The 3D vector object.
  */
 Maths.Vector3 = (X, Y, Z) => {
-    return {
-        x: X,
-        y: Y,
-        z: Z,
-    }
-}
+	return {
+		x: X,
+		y: Y,
+		z: Z,
+	};
+};
 
 /**
  * Represents a 4-dimensional vector.
- * 
+ *
  * @param {number} X - The X component of the vector.
  * @param {number} Y - The Y component of the vector.
  * @param {number} Z - The Z component of the vector.
@@ -186,12 +183,50 @@ Maths.Vector3 = (X, Y, Z) => {
  * @returns {Object} - The 4-dimensional vector object.
  */
 Maths.Vector4 = (X, Y, Z, W) => {
-    return {
-        x: X,
-        y: Y,
-        z: Z,
-        w: W,
-    }
-}
+	return {
+		x: X,
+		y: Y,
+		z: Z,
+		w: W,
+	};
+};
+
+/**
+ * Formats a number as currency with the specified currency code.
+ *
+ * @param {number} Amount - The number to format as currency.
+ * @param {string} CurrencyCode - The currency code to use for formatting (e.g., "USD", "EUR").
+ * @returns {string} The formatted currency string.
+ * @throws {Error} If the Amount parameter is not a number.
+ * @throws {Error} If the CurrencyCode parameter is not a valid currency code.
+ */
+Maths.FormatAsCurrency = (Amount = 0, CurrencyCode = "USD") => {
+	if (typeof Amount !== "number") return InternalLogger.error("Amount must be a number");
+	const validCurrencyCodes = [
+        "USD",
+        "EUR",
+        "GBP",
+        "AUD",
+        "CAD",
+        "JPY",
+        "CHF",
+        "CNY",
+        "SEK",
+        "NZD",
+        "NOK",
+        "MXN",
+        "SGD",
+        "HKD",
+        "KRW",
+        "TRY",
+        "INR",
+        "RUB",
+        "BRL",
+        "ZAR",
+        "PLN",
+	];
+	if (!validCurrencyCodes.includes(CurrencyCode)) InternalLogger.error("Invalid currency code");
+	return Amount.toLocaleString("en-US", { style: "currency", currency: CurrencyCode });
+};
 
 module.exports = { Maths };
